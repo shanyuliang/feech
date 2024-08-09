06d4b2b1e04c7c7ebc91fe1885c216be624bf987
Luke Shan <shanyuliang@gmail.com>
2024-08-09 22:52:25 +1000

    Update animated svg widget

--- lib/src/widgets/animated_svg_widget.dart
+++ lib/src/widgets/animated_svg_widget.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 import '../extensions/alignment_extension.dart';
@@ -64,9 +65,9 @@ class _WebAnimatedSvgWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (svgLink.startsWith("http")) {
-      return Image.network(svgLink);
+      return SvgPicture.network(svgLink);
     } else {
-      return Image.asset(svgLink);
+      return SvgPicture.asset(svgLink);
     }
   }
 }
