6afea9666a5b4519cbd748aa368a9603d08ff84f
Luke Shan <shanyuliang@gmail.com>
2024-08-12 14:19:45 +1000

    Update animated svg widget

--- lib/src/widgets/animated_svg_widget/animated_svg_widget_web.dart
+++ lib/src/widgets/animated_svg_widget/animated_svg_widget_web.dart
@@ -29,9 +29,13 @@ class AnimatedSvgWidget extends ConsumerStatefulWidget {
 
 class _AnimatedSvgWidgetState extends ConsumerState<AnimatedSvgWidget> {
   final _validator = html.NodeValidatorBuilder.common()
-  ..allowSvg();
-    // ..allowElement('img', attributes: ['src'], uriPolicy: _AllowUriPolicy())
-    // ..allowElement('a', attributes: ['href'], uriPolicy: _AllowUriPolicy());
+    ..allowElement("title")
+    ..allowElement("meta")
+    ..allowInlineStyles()
+    ..allowSvg();
+
+  // ..allowElement('img', attributes: ['src'], uriPolicy: _AllowUriPolicy())
+  // ..allowElement('a', attributes: ['href'], uriPolicy: _AllowUriPolicy());
 
   @override
   Widget build(BuildContext context) {
