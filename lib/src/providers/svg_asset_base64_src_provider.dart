cda15c918e1ece2ea2609c62c1868a48be0fd974
Luke Shan <shanyuliang@gmail.com>
2024-08-20 13:34:52 +1000

    Update animated svg widget

--- lib/src/providers/svg_asset_base64_src_provider.dart
+++ lib/src/providers/svg_asset_base64_src_provider.dart
@@ -10,8 +10,7 @@ class SvgAssetBase64SrcProvider extends _$SvgAssetBase64SrcProvider {
   @override
   Future<String> build({required String svgAsset}) async {
     final svgString = await rootBundle.loadString(svgAsset);
-    final svgBase64 = base64Encode(svgString.codeUnits);
-    // final String base64 = base64Encode(utf8.encode(svgString));
+    final svgBase64 = base64Encode(utf8.encode(svgString));
     final base64Src = 'data:image/svg+xml;base64,$svgBase64';
     return base64Src;
   }
