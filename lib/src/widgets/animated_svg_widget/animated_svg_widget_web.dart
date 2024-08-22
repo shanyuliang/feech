624298f14b3ee0925b0342a7bc79bb25f4a0fdb7
Luke Shan <shanyuliang@gmail.com>
2024-08-22 13:31:55 +1000

    Update animated svg widget

--- lib/src/widgets/animated_svg_widget/animated_svg_widget_web.dart
+++ lib/src/widgets/animated_svg_widget/animated_svg_widget_web.dart
@@ -9,19 +9,14 @@ import '../../providers/svg_as_html_string_provider.dart';
 class AnimatedSvgWidget extends ConsumerStatefulWidget {
   final String svgLink;
   final Alignment alignment;
-  final BoxFit fit;
   final Color backgroundColor;
 
   const AnimatedSvgWidget({
     super.key,
     required this.svgLink,
     this.alignment = Alignment.center,
-    this.fit = BoxFit.contain,
     this.backgroundColor = Colors.transparent,
-  }) : assert(
-          fit == BoxFit.contain || fit == BoxFit.cover,
-          'fit can only be contain or cover',
-        );
+  });
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => _AnimatedSvgWidgetState();
