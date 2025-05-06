0da5256701ab1da0249c2bc6186f42a89ce31db1
Luke Shan <shanyuliang@gmail.com>
2025-05-06 22:45:48 +1000

    Update initial_name_avatar.dart

--- lib/src/widgets/initial_name_avatar.dart
+++ lib/src/widgets/initial_name_avatar.dart
@@ -13,7 +13,7 @@ class InitialNameAvatar extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     final initials = fullText?.asNameGetInitials(maxResultLength: maxCharacterCount) ?? '';
-    final color = initials.mapToHSLColor(0.5, 0.5);
+    final color = fullText?.mapToHSLColor(0.5, 0.5);
     final theme = Theme.of(context);
     return roundCornerShape
         ? RoundCornerAvatar(backgroundColor: color, child: Text(initials, style: theme.textTheme.titleMedium?.copyWith(color: Colors.white)))
