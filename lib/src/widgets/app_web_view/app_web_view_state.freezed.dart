f8c4dcfe74d6b30856d6577b0c6439aa9897ad4c
Luke Shan <shanyuliang@gmail.com>
2025-07-07 12:57:01 +1000

    Update dependencies

--- lib/src/widgets/app_web_view/app_web_view_state.freezed.dart
+++ lib/src/widgets/app_web_view/app_web_view_state.freezed.dart
@@ -1,6 +1,5 @@
-// dart format width=80
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
@@ -91,6 +90,136 @@ as StateStatus,
 
 }
 
+
+/// Adds pattern-matching-related methods to [AppWebViewState].
+extension AppWebViewStatePatterns on AppWebViewState {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _AppWebViewState value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _AppWebViewState() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _AppWebViewState value)  $default,){
+final _that = this;
+switch (_that) {
+case _AppWebViewState():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _AppWebViewState value)?  $default,){
+final _that = this;
+switch (_that) {
+case _AppWebViewState() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( int? id,  TextEditingController urlEditorController,  WebViewController webViewController,  String? initialUrl,  String? title,  int? progress,  String? pageStarted,  String? pageFinished,  String? currentUrl,  String? errorMessage,  AppJavaScriptMessage? javaScriptMessage,  AppNavigationDecision? navigationDecision,  bool canGoBack,  bool canGoForward,  bool expandTitleBar,  bool showDebugToolbar,  List<String?> historyUrls,  Map<String?, dynamic> localStorageItems,  Map<String?, dynamic> sessionStorageItems,  StateStatus stateStatus)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _AppWebViewState() when $default != null:
+return $default(_that.id,_that.urlEditorController,_that.webViewController,_that.initialUrl,_that.title,_that.progress,_that.pageStarted,_that.pageFinished,_that.currentUrl,_that.errorMessage,_that.javaScriptMessage,_that.navigationDecision,_that.canGoBack,_that.canGoForward,_that.expandTitleBar,_that.showDebugToolbar,_that.historyUrls,_that.localStorageItems,_that.sessionStorageItems,_that.stateStatus);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( int? id,  TextEditingController urlEditorController,  WebViewController webViewController,  String? initialUrl,  String? title,  int? progress,  String? pageStarted,  String? pageFinished,  String? currentUrl,  String? errorMessage,  AppJavaScriptMessage? javaScriptMessage,  AppNavigationDecision? navigationDecision,  bool canGoBack,  bool canGoForward,  bool expandTitleBar,  bool showDebugToolbar,  List<String?> historyUrls,  Map<String?, dynamic> localStorageItems,  Map<String?, dynamic> sessionStorageItems,  StateStatus stateStatus)  $default,) {final _that = this;
+switch (_that) {
+case _AppWebViewState():
+return $default(_that.id,_that.urlEditorController,_that.webViewController,_that.initialUrl,_that.title,_that.progress,_that.pageStarted,_that.pageFinished,_that.currentUrl,_that.errorMessage,_that.javaScriptMessage,_that.navigationDecision,_that.canGoBack,_that.canGoForward,_that.expandTitleBar,_that.showDebugToolbar,_that.historyUrls,_that.localStorageItems,_that.sessionStorageItems,_that.stateStatus);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( int? id,  TextEditingController urlEditorController,  WebViewController webViewController,  String? initialUrl,  String? title,  int? progress,  String? pageStarted,  String? pageFinished,  String? currentUrl,  String? errorMessage,  AppJavaScriptMessage? javaScriptMessage,  AppNavigationDecision? navigationDecision,  bool canGoBack,  bool canGoForward,  bool expandTitleBar,  bool showDebugToolbar,  List<String?> historyUrls,  Map<String?, dynamic> localStorageItems,  Map<String?, dynamic> sessionStorageItems,  StateStatus stateStatus)?  $default,) {final _that = this;
+switch (_that) {
+case _AppWebViewState() when $default != null:
+return $default(_that.id,_that.urlEditorController,_that.webViewController,_that.initialUrl,_that.title,_that.progress,_that.pageStarted,_that.pageFinished,_that.currentUrl,_that.errorMessage,_that.javaScriptMessage,_that.navigationDecision,_that.canGoBack,_that.canGoForward,_that.expandTitleBar,_that.showDebugToolbar,_that.historyUrls,_that.localStorageItems,_that.sessionStorageItems,_that.stateStatus);case _:
+  return null;
+
+}
+}
+
+}
 
 /// @nodoc
 
