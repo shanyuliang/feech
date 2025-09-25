7deb5e7e8460746d4571e4e93a046eb04be05fb6
Luke Shan <shanyuliang@gmail.com>
2025-09-25 21:43:23 +1000

    feat: Refactor toShortString methods for Alignment and BoxFit extensions using switch expressions

--- lib/src/extensions/alignment_extension.dart
+++ lib/src/extensions/alignment_extension.dart
@@ -1,83 +1,104 @@
 import 'package:flutter/widgets.dart';
 
 extension AlignmentExtension on Alignment {
-  String toShortString() => toString().replaceFirst("Alignment.", "");
+  String toShortString() => switch (this) {
+    Alignment.topLeft => "topLeft",
+    Alignment.topCenter => "topCenter",
+    Alignment.topRight => "topRight",
+    Alignment.centerLeft => "centerLeft",
+    Alignment.center => "center",
+    Alignment.centerRight => "centerRight",
+    Alignment.bottomLeft => "bottomLeft",
+    Alignment.bottomCenter => "bottomCenter",
+    Alignment.bottomRight => "bottomRight",
+    _ => "center",
+  };
 
   String toCSSPosition() => switch (this) {
-        Alignment.topLeft => "left top",
-        Alignment.topCenter => "center top",
-        Alignment.topRight => "right top",
-        Alignment.centerLeft => "left center",
-        Alignment.center => "center center",
-        Alignment.centerRight => "right center",
-        Alignment.bottomLeft => "left bottom",
-        Alignment.bottomCenter => "center bottom",
-        Alignment.bottomRight => "right bottom",
-        _ => "center center",
-      };
+    Alignment.topLeft => "left top",
+    Alignment.topCenter => "center top",
+    Alignment.topRight => "right top",
+    Alignment.centerLeft => "left center",
+    Alignment.center => "center center",
+    Alignment.centerRight => "right center",
+    Alignment.bottomLeft => "left bottom",
+    Alignment.bottomCenter => "center bottom",
+    Alignment.bottomRight => "right bottom",
+    _ => "center center",
+  };
 
   String toCSSMargin() => switch (this) {
-        Alignment.topLeft => '''
+    Alignment.topLeft =>
+      '''
                 margin-left: 0;
                 margin-right: auto;
                 margin-top: 0;
                 margin-bottom: auto;
     ''',
-        Alignment.topCenter => '''
+    Alignment.topCenter =>
+      '''
                 margin-left: auto;
                 margin-right: auto;
                 margin-top: 0;
                 margin-bottom: auto;
     ''',
-        Alignment.topRight => '''
+    Alignment.topRight =>
+      '''
                 margin-left: auto;
                 margin-right: 0;
                 margin-top: 0;
                 margin-bottom: auto;
     ''',
-        Alignment.centerLeft => '''
+    Alignment.centerLeft =>
+      '''
                 margin-left: 0;
                 margin-right: auto;
                 margin-top: auto;
                 margin-bottom: auto;
     ''',
-        Alignment.center => '''
+    Alignment.center =>
+      '''
                 margin-left: auto;
                 margin-right: auto;
                 margin-top: auto;
                 margin-bottom: auto;
     ''',
-        Alignment.centerRight => '''
+    Alignment.centerRight =>
+      '''
                 margin-left: auto;
                 margin-right: 0;
                 margin-top: auto;
                 margin-bottom: auto;
     ''',
-        Alignment.bottomLeft => '''
+    Alignment.bottomLeft =>
+      '''
                 margin-left: 0;
                 margin-right: auto;
                 margin-top: auto;
                 margin-bottom: 0;
     ''',
-        Alignment.bottomCenter => '''
+    Alignment.bottomCenter =>
+      '''
                 margin-left: auto;
                 margin-right: auto;
                 margin-top: auto;
                 margin-bottom: 0;
     ''',
-        Alignment.bottomRight => '''
+    Alignment.bottomRight =>
+      '''
                 margin-left: auto;
                 margin-right: 0;
                 margin-top: auto;
                 margin-bottom: 0;
     ''',
-        _ => '''
+    _ =>
+      '''
                 margin-left: auto;
                 margin-right: auto;
                 margin-top: auto;
                 margin-bottom: auto;
     ''',
-      };
+  };
 
   String toSvgPreserveAspectRatio(String? meetOrSlice) {
     final actionMeetOrSlice = meetOrSlice ?? "meet";
--- lib/src/extensions/box_fit_extension.dart
+++ lib/src/extensions/box_fit_extension.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/widgets.dart';
 
 extension BoxFitExtension on BoxFit {
-  String toShortString() => toString().replaceFirst("BoxFit.", "");
+  String toShortString() => switch (this) {
+    BoxFit.fill => "fill",
+    BoxFit.contain => "contain",
+    BoxFit.cover => "cover",
+    BoxFit.fitWidth => "fitWidth",
+    BoxFit.fitHeight => "fitHeight",
+    BoxFit.scaleDown => "scaleDown",
+    BoxFit.none => "none",
+  };
 }
