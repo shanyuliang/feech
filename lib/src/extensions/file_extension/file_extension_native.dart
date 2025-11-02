071cbd1fc7966d982c338cb5d9d13bb2cf0abbfd
Luke Shan <shanyuliang@gmail.com>
2025-11-03 08:35:09 +1000

    feat: Refactor isSvgFile method in FileExtension to improve error handling and code clarity

--- lib/src/extensions/file_extension/file_extension_native.dart
+++ lib/src/extensions/file_extension/file_extension_native.dart
@@ -6,11 +6,14 @@ import '../../utilities/handy_util.dart';
 
 extension FileExtension on File {
   Future<bool> isSvgFile() async {
-    return suppressThrowableAsyncDefault(throwable: () async {
-      (await vg.loadPicture(SvgFileLoader(this), null)).picture.dispose();
-      return true;
-    }, whenError: (error, stackTrace) async {
-      return false;
-    });
+    return suppressThrowableAsyncDefault(
+      throwable: () async {
+        (await vg.loadPicture(SvgFileLoader(this), null)).picture.dispose();
+        return true;
+      },
+      whenError: (error, stackTrace) async {
+        return false;
+      },
+    );
   }
 }
