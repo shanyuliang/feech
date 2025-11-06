9fcb376b1dd096e399f87141c54062d8f4d9d971
Luke Shan <shanyuliang@gmail.com>
2025-11-06 20:53:35 +1000

    feat: Update isSvgFile method in FileExtension to accept Object type and simplify implementation

--- lib/src/extensions/file_extension/file_extension_web.dart
+++ lib/src/extensions/file_extension/file_extension_web.dart
@@ -1,18 +1,5 @@
-import 'package:flutter_cache_manager/file.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import '../../utilities/handy_util.dart';
-
-extension FileExtension on File {
+extension FileExtension on Object {
   Future<bool> isSvgFile() async {
-    return suppressThrowableAsyncDefault(
-      throwable: () async {
-        (await vg.loadPicture(SvgFileLoader(this), null)).picture.dispose();
-        return true;
-      },
-      whenError: (error, stackTrace) async {
-        return false;
-      },
-    );
+    return false;
   }
 }
