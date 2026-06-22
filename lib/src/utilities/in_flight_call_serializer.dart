0eb1c12c65a4a7d8251685fe210952ac055b7de9
Luke Shan <shanyuliang@gmail.com>
2026-06-22 23:50:28 +1000

    refactor: Improve documentation in InFlightCallSerializer with additional usage examples

--- lib/src/utilities/in_flight_call_serializer.dart
+++ lib/src/utilities/in_flight_call_serializer.dart
@@ -2,6 +2,9 @@
  * A utility class to serialize in-flight calls based on a key. If a call with the same key is already in-flight, it returns the existing Future instead of starting a new one.
  * Usage example:
  * final _inFlightCallSerializer = InFlightCallSerializer();
+ * ...
+ * ...
+ * ...
  * final result = await _inFlightCallSerializer.run<String?>('getAccessToken', _fetchAccessToken);
  */
 class InFlightCallSerializer {
