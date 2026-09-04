d5f8febf8a821433c9a266854d2dd5fda0deea2d
Luke Shan <shanyuliang@gmail.com>
2026-09-04 21:02:26 +1000

    fix: Update flutter_riverpod and flutter_soloud dependency versions in pubspec.yaml

--- lib/src/extensions/palette_generator_extension.dart
+++ lib/src/extensions/palette_generator_extension.dart
@@ -1,20 +1,19 @@
 import 'package:material_ui/material_ui.dart';
-import 'package:palette_generator/palette_generator.dart';
 
-extension PaletteGeneratorExtension on PaletteGenerator {
-  List<Color> toGradientColors({int maxColor = 5}) {
-    paletteColors.sort((PaletteColor a, PaletteColor b) {
-      return b.population.compareTo(a.population);
-    });
-    switch (paletteColors.length) {
-      case 0:
-        return [Colors.transparent, Colors.transparent];
-      case 1:
-        return [paletteColors[0].color, paletteColors[0].color];
-      default:
-        return [
-          ...paletteColors.map((e) => e.color).take(maxColor),
-        ];
-    }
-  }
-}
+// extension PaletteGeneratorExtension on PaletteGenerator {
+//   List<Color> toGradientColors({int maxColor = 5}) {
+//     paletteColors.sort((PaletteColor a, PaletteColor b) {
+//       return b.population.compareTo(a.population);
+//     });
+//     switch (paletteColors.length) {
+//       case 0:
+//         return [Colors.transparent, Colors.transparent];
+//       case 1:
+//         return [paletteColors[0].color, paletteColors[0].color];
+//       default:
+//         return [
+//           ...paletteColors.map((e) => e.color).take(maxColor),
+//         ];
+//     }
+//   }
+// }
